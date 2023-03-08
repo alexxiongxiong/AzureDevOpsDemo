9b021e060c3e6c2c0abb93f9c27f1177b8ba6723
Xinhao Xiong <xinhaoxiong@microsoft.com>
2023-03-08 09:05:27 +0000

    Updated check_deployment_existing.sh

--- EnvironmentSetting/check_deployment_existing.sh
+++ EnvironmentSetting/check_deployment_existing.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 deployment_name=goweb-deployment
 namespace_name=azuredevopsapp
-
-kubectl get deployment -n ${namespace_name} | grep -i ${deployment_name} &> /dev/null
-
+kubectl get deployment -n ${namespace_name} | grep -i ${deployment_name}
 if [ $? -eq 0 ]; then
 kubectl delete deployment ${deployment_name} -n ${namespace_name}
 else
